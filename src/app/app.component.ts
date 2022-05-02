0486a31c934625f4eb485ed39ecf72402e0ad489
iman <inaseri.20@gmail.com>
2022-05-02 14:52:57 +0430

    Add some changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { AddToHomeScreenComponent } from './share/components/add-to-home-screen/add-to-home-screen.component';
 
 @Component({
   selector: 'app-root',
@@ -8,24 +7,8 @@ import { AddToHomeScreenComponent } from './share/components/add-to-home-screen/
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'منتورینگ آنلاین';
-  screenWidth: any;
+  title = 'ERP-PROJECTS';
 
   constructor(private dialog: MatDialog) {
-  }
-
-  ngOnInit(): void {
-    this.getScreenSize();
-    if (!window.matchMedia('(display-mode: standalone)').matches && this.screenWidth < 600) {
-      this.dialog.open(AddToHomeScreenComponent, {
-        minWidth: '100%',
-        height: '100%'
-      });
-    }
-  }
-
-  @HostListener('window:resize', ['$event'])
-  getScreenSize(event?: any) {
-    this.screenWidth = window.innerWidth;
   }
 }
