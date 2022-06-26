0f55ae44b74c0908661cd3a701ccb8370c712d45
inaseri <46320303+inaseri@users.noreply.github.com>
2022-06-26 13:34:34 +0430

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.erp.vidone.ir/api/'
+  apiUrl: 'https://api.erp.vidone.ir/api/',
+  apiKey: 'Iman.3802!ExistCodeErpFrontApp'
 };
